77e152115a4ec6515d3713c0ea7127e57ce6bb97
Mazenn3 <167587407+Mazenn3@users.noreply.github.com>
2024-04-23 00:43:36 +0200

    Update Ticket.java

--- project/src/project/Ticket.java
+++ project/src/project/Ticket.java
@@ -1,32 +1,34 @@
 package project;
-import java.util.*;
-public class Ticket {
-    private String ticketID;
-    private Passenger passenger;
-    private Train train;
-    private Station departurestaion;
-    private Station arrivalstaion;
-    private int seatNumber;
-    private double price; 
-    private DateTime departureTime;
 
-    public Ticket(String ticketID, Passenger passenger, Train train, Station departurestaion, Station arrivalstaion, int seatNumber, double price, DateTime departureTime) {
-        this.ticketID = ticketID;
+import java.util.*;
+/**
+ *
+ * @author LENOVO
+ */
+public class Ticket  {
+    // Declaring Variables
+private String TicketId; private Passenger passenger; private Train train;
+private Station departurestation; private Station arrivalstation;
+private int seatNumber;private double price; private Date departureTime;
+private String type; // Ticket type (e.g., "Standard", "First Class", "Economy")
+  // Constructor to define attributes
+    public Ticket(String TicketId, Passenger passenger, Train train, Station departurestation, Station arrivalstation, int seatNumber, double price, Date departureTime) {
+        this.TicketId = TicketId;
         this.passenger = passenger;
         this.train = train;
-        this.departurestaion = departurestaion;
-        this.arrivalstaion = arrivalstaion;
+        this.departurestation = departurestation;
+        this.arrivalstation = arrivalstation;
         this.seatNumber = seatNumber;
         this.price = price;
         this.departureTime = departureTime;
     }
-
-    public String getTicketID() {
-        return ticketID;
+  // setters and getters 
+    public String getTicketId() {
+        return TicketId;
     }
 
-    public void setTicketID(String ticketID) {
-        this.ticketID = ticketID;
+    public void setTicketId(String TicketId) {
+        this.TicketId = TicketId;
     }
 
     public Passenger getPassenger() {
@@ -45,20 +47,20 @@ public class Ticket {
         this.train = train;
     }
 
-    public Station getDeparturestaion() {
-        return departurestaion;
+    public Station getDeparturestation() {
+        return departurestation;
     }
 
-    public void setDeparturestaion(Station departurestaion) {
-        this.departurestaion = departurestaion;
+    public void setDeparturestation(Station departurestation) {
+        this.departurestation = departurestation;
     }
 
-    public Station getArrivalstaion() {
-        return arrivalstaion;
+    public Station getArrivalstation() {
+        return arrivalstation;
     }
 
-    public void setArrivalstaion(Station arrivalstaion) {
-        this.arrivalstaion = arrivalstaion;
+    public void setArrivalstation(Station arrivalstation) {
+        this.arrivalstation = arrivalstation;
     }
 
     public int getSeatNumber() {
@@ -77,19 +79,29 @@ public class Ticket {
         this.price = price;
     }
 
-    public DateTime getDepartureTime() {
+    public Date getDepartureTime() {
         return departureTime;
     }
 
-    public void setDepartureTime(DateTime departureTime) {
+    public void setDepartureTime(Date departureTime) {
         this.departureTime = departureTime;
     }
+     public String Ticket_Details(){
+     return  "ID:"+TicketId +"departureTime:"+ departureTime +"seatNumber:"+ seatNumber +"price:"+ price +"From"+ departurestation +"to"+ arrivalstation
+       +"passenger:"+ passenger+"Train:"+ train;
+     }
+/**********public double CalcPrice(){return 0;};*/
 
-
-
-
-      public void TicketDetails(){
-      System.out.println(" ");
-  }
-  
+    public double CalcPrice() {
+        // Calculate price based on ticket type
+        switch (this.type.toLowerCase()) {
+            case "standard":
+                return 50.0;
+            case "first class":
+                return 70.0;
+            case "economy":
+                return 30.0;
+            default:
+                return 0.0;
+        }}
 }
